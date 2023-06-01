0fa60dfdbf99930914edbda1a65f77cd0b6a436e
Diego Barros <diegovlbarros@gmail.com>
2023-06-01 20:56:00 -0300

    dynamis bar

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import './styles.css';
-import { ProductDTO } from '../../models/product';
-
+import React from "react";
+import "./styles.css";
+import { ProductDTO } from "../../models/product";
+import { Link } from "react-router-dom";
 
 type Props = {
   product: ProductDTO;
 };
 
-export default function CatalogCard(product: any){
-   return (
-    <>
-       <div className="dsc-card">
-         <div className="dsc-catalog-card-top dsc-line-bottom">
-           <img src={product.imgUrl} alt={product.name} /> 
-         </div>
-         <div className="dsc-catalog-card-bottom">
-           <h3>{product.price.toFixed(2)}</h3>
-           <h4>{product.name}</h4>
-         </div>
-       </div> 
-    </>
-   );
+export default function CatalogCard(product: any) {
+  return (
+    <Link to={`/product-details/${product.id}`}>
+      <div className="dsc-card">
+        <div className="dsc-catalog-card-top dsc-line-bottom">
+          <img src={product.imgUrl} alt={product.name} />
+        </div>
+        <div className="dsc-catalog-card-bottom">
+          <h3>{product.price.toFixed(2)}</h3>
+          <h4>{product.name}</h4>
+        </div>
+      </div>
+    </Link>
+  );
 }
